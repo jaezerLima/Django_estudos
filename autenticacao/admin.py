882860548f554e4f8924487639fa9782969d99ae
Jaezer Lima <jaezer@4blue.com.br>
2023-02-21 17:46:08 -0300

    Atributos de exibição

--- autenticacao/admin.py
+++ autenticacao/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Pessoa, Cargos
 # Register your models here.
+
+
+@admin.register(Pessoa)
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'senha')
+    readonly_fields = ('senha', 'cargo')
+    search_fields = ('nome', 'senha')
+    list_filter = ('cargo',)
+    list_editable = ('email',)
+
+admin.site.register(Cargos)

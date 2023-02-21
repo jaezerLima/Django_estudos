9059da0207fdd659bdc1ab5da1489a6bbfa6177e
Jaezer Lima <jaezer@4blue.com.br>
2023-02-21 17:54:22 -0300

    Inline

--- autenticacao/admin.py
+++ autenticacao/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
-from .models import Pessoa, Cargos
+from .models import Pessoa, Cargos, Pedido
 # Register your models here.
+
+
+class PedidoInline(admin.TabularInline):
+    readonly_fields = ('nome', 'quantidade', 'descricao')
+    list_display = ('nome', 'quantidade', 'descricao')
+    model = Pedido
+    extra = 0
 
 
 @admin.register(Pessoa)
 class PessoaAdmin(admin.ModelAdmin):
+    inlines = [PedidoInline]
     list_display = ('nome', 'email', 'senha')
-    readonly_fields = ('senha', 'cargo')
+    # readonly_fields = ('senha', 'cargo')
     search_fields = ('nome', 'senha')
     list_filter = ('cargo',)
     list_editable = ('email',)
+
 
 admin.site.register(Cargos)
--- autenticacao/models.py
+++ autenticacao/models.py
@@ -16,3 +16,13 @@ class Pessoa(models.Model):
 
     def __str__(self):
         return self.nome
+
+
+class Pedido(models.Model):
+    nome = models.CharField(max_length=100)
+    quantidade = models.IntegerField()
+    descricao = models.TextField()
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
